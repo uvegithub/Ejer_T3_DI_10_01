b8836199bbba89c07d40f364ee181cf5d82e12d4
Vanesa <vanesanunez90dos@gmail.com>
2024-01-10 20:47:32 +0100

    10.1 actu

---
+++ app/src/main/java/com/example/ejer_t3_di_10_01/Contacto.kt
@@ -0,0 +1,3 @@
+package com.example.ejer_t3_di_10_01
+
+data class Contacto(val foto: String = "Sin foto", val nombre: String = "Lucia", val telefono: Int = 111111, val correo: String = "a@gmail.com")
